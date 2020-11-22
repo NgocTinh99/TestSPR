1e60a3d4280632db839c4a50e4e85d825ab772c7
trinhngoctinh15101999 <55981579+trinhngoctinh15101999@users.noreply.github.com>
2020-11-22 19:57:46 +0700

    commit

---
+++ src/main/java/ou/trinhngoctinh/QuanLyBanHang/entity/ProductEntity.java
@@ -0,0 +1,16 @@
+package ou.trinhngoctinh.QuanLyBanHang.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table (name ="product")
+public class ProductEntity {
+    @Id  //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//id tự động tăng
+    private int id;
+
+    @Column(name = "product_name")
+    private String productName;
+}
+
