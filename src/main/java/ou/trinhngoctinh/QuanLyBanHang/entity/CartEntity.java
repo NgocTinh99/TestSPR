7095c3295cd58f5256c24e1c921def0dd93b947d
trinhngoctinh15101999 <55981579+trinhngoctinh15101999@users.noreply.github.com>
2020-11-24 15:31:34 +0700

    create entity

--- src/main/java/ou/trinhngoctinh/QuanLyBanHang/entity/CartEntity.java
+++ src/main/java/ou/trinhngoctinh/QuanLyBanHang/entity/CartEntity.java
@@ -3,12 +3,24 @@ package ou.trinhngoctinh.QuanLyBanHang.entity;
 import javax.persistence.*;
 
 @Entity
-@Table (name ="cart")
+@Table (name="cart")
 public class CartEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column (name = "cart_name")
-    private String careName;
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="product_name")
+    private String productName;
+
+    @Column(name="product_repository_name")
+    private String productRepositoryName;
+
+    @Column(name="amount_items")
+    private Integer amountItems;
+
+    @Column(name="total_bill")
+    private Integer totalBill;
 }
